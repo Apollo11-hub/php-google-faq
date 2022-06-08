c6d60602ab03e223e86218b685f9f2a806842fe1
Apollo11-hub <Antonio-Pollo@outlook.it>
2022-06-08 16:38:53 +0200

    fine-php inizio style

--- index.php
+++ index.php
@@ -12,13 +12,25 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="style.css" rel="stylesheet" type="text/css">
   <title>Domande Frequenti - Pezzotto</title>
 </head>
 <body>
-    <header></header>
+    <header>
+      <img src="./Img/Immagine 2022-06-08 162224.png" alt="img:)">
+      <nav>
+        <ul>
+          <li>introduzione</li>
+          <li>norme sulla privacy</li>
+          <li>termini di servizio</li>
+          <li>tecnologie</li>
+          <li>domande frequenti</li>
+        </ul>
+      </nav>
+    </header>
     <main>
 
-      <div>
+      <div class="wrapper-main">
         <?php foreach($faq_data as $faqs => $faq): ?>
 
             <h2><?php echo $faq['question']?> </h2>
